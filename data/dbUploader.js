8e460e5e9487d340ddcce9519151baa89b3e402d
Alexander Beischl <alexander.beischl@gmx.de>
2016-03-07 19:53:06 +0100

    Removed some obsolete comments

--- data/dbUploader.js
+++ data/dbUploader.js
@@ -28,43 +28,21 @@ for(i = 0; i < allCities.length; i++)
         console.log(chars);
     });
 	/*
-	jQuery.post("https://got-api.bruck.me/api/city", 
-				{name : "King's Landing",
-				 coordX: '-84.48536531890602', 
-				 coordY:'45.805474068515245', 
-				 type:"city", 
-				 priority: 2, 
-				 link: "http://awoiaf.westeros.org/index.php/King%27s_Landing"}, 
-				 function(data){
-			chars = data;
-			console.log(chars);
-		});
-
 	
-	
-	console.log( {	name: city.name,
-								coordX: city.coord.lng,
-								coordY: city.coord.lat,
-								type: city.type,
-								priority: parseInt(city.prio),
-								link: city.link
-						});
 		jQuery.ajax("https://got-api.bruck.me/api/api/city",
-						{
-							type: 'POST',
-							data: {	name: city.name,
-								coordX: city.coord.lng,
-								coordY: city.coord.lat,
-								type: city.type,
-								priority: parseInt(city.prio),
-								link: city.link
-							},
-							success: function(data)
-										{
-											chars = data;
-											console.log(chars);
-										}
-						}
-					);*/	
+		{
+			type: 'POST',
+			data: {	name: city.name,
+			coordX: city.coord.lng,
+			coordY: city.coord.lat,
+			type: city.type,
+			priority: parseInt(city.prio),
+			link: city.link },
+			success: function(data)
+			{
+				chars = data;
+				console.log(chars);
+			}
+		});*/	
 }
 
