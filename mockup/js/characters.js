501928c30977f23b7ed35df093b8527a7b930f7c
Maximilian Bandle <bandle@in.tum.de>
2016-03-07 17:24:42 +0100

    Markers with Character Images

--- mockup/js/characters.js
+++ mockup/js/characters.js
@@ -11,7 +11,7 @@ jQuery(function() {
 		'#03A9F4', '#795548', '#E91E63', '#FF5722'];
 	personList.map(function(p,i){ // For every person
 		var img = p.img || defaultPersonImage; // Image defined or use default
-		color = colors[i%colors.length]; // Iterate through the colors
+		var color = colors[i%colors.length]; // Iterate through the colors
 		// Make new elem
 		var item = $('<div class="character disabled" id="character'+i+'" ><img src="'+img+'"'+
 		'class="img-circle" style="border-color:'+color+'"/>'+
@@ -43,11 +43,10 @@ jQuery(function() {
 			var el = $(e.target); // Clicked Element
 			if(!el.hasClass('character')) {
 				el = el.parents('.character'); // Get the container
-				$("#dynModal").modal("show");
+			//	$("#dynModal").modal("show");
 			}
 			el.toggleClass('disabled'); // Toggle the class name (de-)activate it
-			alert("Function reached");
-			mapHelpers.characterPins(el.find(".name").text());
+			mapHelpers.characterPins(p, color);
 		});
 		$("#characters").append(item);// Add it to the list
 		
