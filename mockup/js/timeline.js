0fa34297c900abc320c8a8df8574c4a68ee6c7ca
Tobias Piffrader <ga54biq@mytum.de>
2016-03-07 15:23:47 +0100

    timeline implementation + some episodes for the timeline

--- mockup/js/timeline.js
+++ mockup/js/timeline.js
@@ -7,5 +7,28 @@
                         :_;
 */
 jQuery(function() {
+	var selected = "0-1";	
+	
 	$('#timeline').text("Here comes the timeline");
+	
+	$("#slider-range").slider({
+        range: true,
+        min: 0,
+        max: 49,
+        values: [0, 1],
+        slide: function(event, ui) {
+            selected = [ui.values[0], ui.values[1]];
+            if (selected[0] == selected[1]) {
+                selected = selected[0];
+            } else {
+	            selected = selected[0] +"-"+ selected[1];
+            }
+            $("#amount").text(getEpisodeInfo(ui.values[0]) + " - " + getEpisodeInfo(ui.values[1]));
+        }
+    });
+    
+        function getEpisodeInfo(i) {
+        var e = episodes[i];
+        return "S" + e.season + "E" + e.episode + ": " + e.title;
+    }
 });
