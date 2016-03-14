8a4e42904da173314016c65264c21adc879fc5a8
Maximilian Bandle <bandle@in.tum.de>
2016-03-14 10:47:08 +0100

    Fixed all character-links

--- data/characters.js
+++ data/characters.js
@@ -16,9 +16,9 @@ personList = {
 	'Brandon Stark':{img:'img/persons/bran_stark.jpg', link:'http://awoiaf.westeros.org/index.php/Brandon_Stark'},
 	'Rickon Stark':{img:'img/persons/rickon_stark.jpg', link:'http://awoiaf.westeros.org/index.php/Rickon_Stark'},
 	'Jon Snow':{img:'img/persons/jon_snow.jpg', link:'http://awoiaf.westeros.org/index.php/Jon_Snow'},
-	'56d912fb1e3cd5e6474a28a7':{name:'Daenerys Targaryen', img:'img/persons/daenerys_targaryen.jpg', house:'House Targaryen', link:'http://awoiaf.westeros.org/index.php/Daenerys_Targaryen'},
-	'56d912fc1e3cd5e6474a2e77':{name:'Tywin Lannister', img:'img/persons/tywin_lannister.png', house:'House Lannister', link:'http://awoiaf.westeros.org/index.php/Tywin_Lannister'},
-	'56d912fb1e3cd5e6474a2a9c':{name:'Jaime Lannister', img:'img/persons/jaime_lannister.png', house:'House Lannister', link:'http://awoiaf.westeros.org/index.php/Jaime_Lannister'},
-	'56d912fb1e3cd5e6474a2865':{name:'Cersei Lannister', img:'img/persons/cersei_lannister.jpeg', house:'House Lannister', link:'http://awoiaf.westeros.org/index.php/Cersei_Lannister'},
-	'56d913b61e3cd5e6474a35b1':{name:'Tyrion Lannister', img:'img/persons/tyrion_lannister.png', house:'House Lannister', link:'http://awoiaf.westeros.org/index.php/Tyrion_Lannister'}
+	'Daenerys Targaryen':{img:'img/persons/daenerys_targaryen.jpg', link:'http://awoiaf.westeros.org/index.php/Daenerys_Targaryen'},
+	'Tywin Lannister':{img:'img/persons/tywin_lannister.png', link:'http://awoiaf.westeros.org/index.php/Tywin_Lannister'},
+	'Jaime Lannister':{img:'img/persons/jaime_lannister.png', link:'http://awoiaf.westeros.org/index.php/Jaime_Lannister'},
+	'Cersei Lannister':{img:'img/persons/cersei_lannister.jpeg', link:'http://awoiaf.westeros.org/index.php/Cersei_Lannister'},
+	'Tyrion Lannister':{img:'img/persons/tyrion_lannister.png', link:'http://awoiaf.westeros.org/index.php/Tyrion_Lannister'}
 };
