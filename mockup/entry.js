04dfea1a2e1fe27074961c89c33a85fc43cd41b9
Oleksii Moroz <oleksii.moroz@tum.de>
2016-03-15 11:38:50 +0100

    html added to bundle

--- mockup/entry.js
+++ mockup/entry.js
@@ -1,11 +1,11 @@
 
 require('expose?$!expose?jQuery!jquery');
 require("../node_modules/bootstrap/dist/css/bootstrap.min.css");
-require("../node_modules/bootstrap/dist/js/bootstrap.min.js");
 require("./lib/jquery-ui.min.js");
+require("../node_modules/bootstrap/dist/js/bootstrap.min.js");
 
 //html
-//require("html!./mockup.html");
+require("html!../map.html");
 
 //include css files + images
 require("./css/leaflet.css");
@@ -14,7 +14,6 @@ require("./css/main.css");
 //include libraries
 require("./lib/leaflet.js");
 require("./lib/label.js");
-require.context("./lib", true, /\.js$/);
 require("./lib/leaflet.label/BaseMarkerMethods.js");
 require("./lib/leaflet.label/FeatureGroup.Label.js");
 require("./lib/leaflet.label/Map.Label.js");
@@ -22,7 +21,7 @@ require("./lib/leaflet.label/Marker.Label.js");
 require("./lib/leaflet.label/Path.Label.js");
 
 // include js files
-require("./js/filter.js");
 require("./js/helper.js");
 require("./js/map.js");
+require("./js/filter.js");
 require("./js/timeline.js");
