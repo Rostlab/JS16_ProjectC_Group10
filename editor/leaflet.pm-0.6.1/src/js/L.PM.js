f025a553bcb512d3d59244fd7dba5b774d536c42
Alexander Beischl <alexander.beischl@gmx.de>
2016-03-17 22:50:00 +0100

    Updated and redesigned the Editor, we are using a leaflet plugin instead of our own editor now. It has improved in usability :)

---
+++ editor/leaflet.pm-0.6.1/src/js/L.PM.js
@@ -0,0 +1,33 @@
+/**
+*
+* A Leaflet Plugin For Editing Geometry Layers in Leaflet 1.0
+* by Sumit Kumar (@TweetsOfSumit)
+* Github Repo: https://github.com/codeofsumit/leaflet.pm
+*/
+
+L.PM = L.PM || {
+    initialize: function() {
+
+        var initLayerGroup = function() {
+            this.pm = new L.PM.Edit.LayerGroup(this);
+        };
+        L.LayerGroup.addInitHook(initLayerGroup);
+
+
+        var initPolygon = function() {
+            this.pm = new L.PM.Edit.Poly(this);
+        };
+        L.Polygon.addInitHook(initPolygon);
+
+
+        var initMap = function() {
+            this.pm = new L.PM.Draw(this);
+        };
+        L.Map.addInitHook(initMap);
+
+    },
+    Edit: {}
+};
+
+// initialize leaflet.pm
+L.PM.initialize();
