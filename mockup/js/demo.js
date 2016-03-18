a986b2932c7c8755e829c0d215430ff0f3177316
Maximilian Bandle <bandle@in.tum.de>
2016-03-18 14:14:32 +0100

    Untracked Files

---
+++ mockup/js/demo.js
@@ -0,0 +1,21 @@
+/*____     _____   __  __
+ / ___| __|_   _| |  \/  | __ _ _ __
+| |  _ / _ \| |   | |\/| |/ _` | '_ \
+| |_| | (_) | |   | |  | | (_| | |_) |
+ \____|\___/|_|   |_|  |_|\__,_| .__/
+ Maximilian Bandle @mbandle    |_|
+ Alexander Beischl @AlexBeischl
+ Tobias Piffrader  @tpiffrader
+
+Run It
+
+*/
+jQuery(function() {
+	mymap = gotmap('#map', {
+		'characterBox':'#characters',
+		'timeline':'#timeline',
+		'filter':'#filter input',
+		'cityDataSource':'https://raw.githubusercontent.com/Rostlab/JS16_ProjectC_Group10/develop/data/cities.js',
+		'realmDataSource':'https://raw.githubusercontent.com/Rostlab/JS16_ProjectC_Group10/develop/data/realms.js'
+	});
+});
