6df957b540b40dda88001b1d597dae06472e03aa
Tobias Piffrader <ga54biq@mytum.de>
2016-04-04 15:43:27 +0200

    formatting

--- editor/js/community-editor.js
+++ editor/js/community-editor.js
@@ -257,8 +257,9 @@ jQuery(function() {
 		function (data){
 			var allEpisodes = (typeof data == "object") ? data : JSON.parse(data);
 			
-			allEpisodes.sort(function(a, b)
-    			{return a.totalNr - b.totalNr;});	
+			allEpisodes.sort(function(a, b) {
+    			return a.totalNr - b.totalNr;
+			});
 			var episodesCount = allEpisodes.length;
 			allEpisodes.map(function (episode) {
 				episode.showTitle = "S" + episode.season +"E"+episode.nr+": " + episode.name;
