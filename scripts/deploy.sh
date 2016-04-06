befb1b4428439af61aa99d9b59d8430184ea97bb
AlexMoroz <oleksii.moroz@tum.de>
2016-04-06 20:14:32 +0200

    message changed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,8 +7,8 @@ mkdir builds
 
 
 # config
-git config user.email "oleksii.moroz@tum.de"
-git config user.name "AlexMoroz"
+git config user.email "nobody@nobody.com"
+git config user.name "Travis CI"
 
 # build
 npm run build
@@ -17,5 +17,5 @@ npm run deploy
 # deploy
 git checkout $TRAVIS_BRANCH
 git add .
-git commit -m"Travis automatic deployment"
+git commit -m"New bundles created"
 git push "https://${GITHUB_TOKEN}@github.com/Rostlab/JS16_ProjectC_Group10.git" $TRAVIS_BRANCH
