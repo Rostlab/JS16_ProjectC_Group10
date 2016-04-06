b947293e5b552d1288de3c3ccb21fbaa2c1d3839
Oleksii Moroz <oleksii.moroz@tum.de>
2016-04-06 12:54:09 +0200

    test deployment changes

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+set -o errexit
+
+rm -rf builds
+mkdir builds
+
+# config
+git config --global user.email "nobody@nobody.org"
+git config --global user.name "Travis CI"
+
+# build
+npm run build
+npm run deploy
+
+# deploy
+git add .
+git commit -m "Travis automatic deployment"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" develop > /dev/null 2>&1
