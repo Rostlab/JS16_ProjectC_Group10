2ac792fff0bc0ebf2b3a162535ffb5b5f3cdc8da
AlexMoroz <oleksii.moroz@tum.de>
2016-04-08 23:49:17 +0200

    small improvement of deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,12 +9,15 @@ mkdir builds
 git config user.email "nobody@nobody.com"
 git config user.name "Travis CI"
 
+# pull
+git checkout $TRAVIS_BRANCH
+git pull
+
 # build
 npm run build
 npm run deploy
 
 # deploy
-git checkout $TRAVIS_BRANCH
 git add .
 git commit -m"New bundles created"
 git push -q "https://${GITHUB_TOKEN}@github.com/Rostlab/JS16_ProjectC_Group10.git" $TRAVIS_BRANCH
